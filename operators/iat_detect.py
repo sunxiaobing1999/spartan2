4172827d39e01d60131bb7c370b9c437b31320d1
sunxiaobing <759677946@qq.com>
2020-08-18 15:36:59 +0800

    modify iat operator and add iat_detect operator.

--- operators/iat_detect.py
+++ operators/iat_detect.py
@@ -1,11 +1,16 @@
-# -*- coding:utf-8 -*-
-import argparse
-import sys
-sys.path.append('/Users/baby/Documents/GitHub/spartan2/')
+#!/usr/bin/env python
+# encoding: utf-8
+# @author: Sunxiaobing
+# @file: iat_detect.py
+# @time: 2020/8/18
+# @version v1.0
+# @desc:
+#
 
-import spartan2.basicutil as iatutil
-import spartan2.drawutil as drawutil
-import spartan2.ioutil as ioutil
+import argparse
+import sys, os
+import pickle
+import spartan as st
 import pandas as pd
 
 if __name__ == "__main__":
@@ -14,14 +19,6 @@ if __name__ == "__main__":
                         help="输入文件名")
     parser.add_argument("--gridsize", type=int, default=100,
                         help="Histogram的网格数，示例格式:100")
-    parser.add_argument("--delimeter", type=str, default='\x01',
-                        help="输入文件分割符;示例1: , 示例2: \x01")
-    parser.add_argument("--timeformat", type=str, default='%Y-%m-%d %H:%M:%S',
-                        help="输入数据的时间格式;示例数据%Y-%m-%d %H:%M:%S")
-    parser.add_argument("--timeidx", type=int, default=0,
-                        help="时间列的id(id从0开始);示例数据：0 (表示第一列)")
-    parser.add_argument("--groupids", nargs='*', type=list, default=[0],
-                        help=")时间列的id(id从0开始);示例数据：0 (表示第一列)")
     parser.add_argument("--xlabel", type=str, default='IATn',
                         help="Histogram横坐标的标签")
     parser.add_argument("--ylabel", type=str, default='IATn+1',
@@ -32,38 +29,43 @@ if __name__ == "__main__":
                         help="Histogram查询点的纵坐标")
     parser.add_argument("--radius", type=float, default=1,
                         help="Histogram的查询半径")
-    parser.add_argument('-o1', "--outfig", nargs='?', type=str, default='/source/out.jpg',
-                    help="图片的输出文件")
-    parser.add_argument('-o2', "--outfile", type=str, default='/source/user.csv',
+    parser.add_argument("--k", type=int, default=10,
+                        help="前k个可疑用户")
+    parser.add_argument('-o1', "--outfig", nargs='?', type=str, default='./source/out.jpg',
+                    help="Histogram图的输出文件")
+    parser.add_argument('-o2', "--outfile", type=str, default='./source/user.csv',
                     help="用户的输出文件")
+    parser.add_argument('-o3', "--outpdf", type=str, default='./source/pdf.jpg',
+                    help="top-k用户的iat分布图的输出文件")
 
     args = parser.parse_args()
 
     argsinput = args.input
     argsgridsize = args.gridsize
-    argsdelimeter = args.delimeter
-    argstimeformat = args.timeformat
-    argstimeidx = args.timeidx
-    argsgroupids = args.groupids
     argsxlabel = args.xlabel
     argsylabel = args.ylabel
  
     argsx = args.x
     argsy = args.y
     argsradius = args.radius
+    argsk = args.k
     argsoutfig = args.outfig
     argsoutfile = args.outfile
+    argsoutpdf = args.outpdf
 
-    aggts = ioutil.extracttimes(argsinput, outfile=None, timeidx=argstimeidx, timeformat=argstimeformat, delimeter=argsdelimeter,
-                                isbyte=True, comments='#', nodetype=str, groupids=argsgroupids)
-    instance = iatutil.IAT()
-    instance.calaggiat(aggts)
+    with open(argsinput, 'rb') as picklefile:
+        instance = pickle.load(picklefile)
+        picklefile.close()
+
     xs, ys = instance.getiatpairs()
 
     # rect: instance of class RectHistogram
-    rect = drawutil.RectHistogram()
+    rect = st.RectHistogram(xscale='log', yscale='log', gridsize=argsgridsize)
     fig, H, xedges, yedges = rect.draw(xs, ys, outfig=argsoutfig, xlabel=argsxlabel, ylabel=argsylabel)
+
     iatpairs = rect.find_peak_rect(xs, ys, H, xedges, yedges, x=argsx, y=argsy, radius=argsradius)
-    usrlist = instance.find_iatpair_user(iatpairs)
+    usrlist = instance.find_iatpair_user_ordered(iatpairs, k=argsk)
     df = pd.DataFrame({'user':usrlist})
     df.to_csv(argsoutfile, index=False)
+
+    instance.drawIatPdf(usrlist, outfig=argsoutpdf)
